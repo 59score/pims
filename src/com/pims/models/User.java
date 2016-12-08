a2c801ba84d2652376e09f020e42768fe7d67be9
Rita Liang <viso.liang@qq.com>
2016-12-08 20:55:59 +0800

    架构个人信息管理系统的M端。

--- src/com/pims/models/User.java
+++ src/com/pims/models/User.java
@@ -1,10 +1,22 @@
 package com.pims.models;
 
 public class User {
+	
+	private Integer id;
 	private String username;
 	private String password;
+	private String name;
+	private String sex;
+	private String birth;
+	private String nation;
+	private String edu;
+	private String work;
+	private String phone;
+	private String place;
+	private String email;
 
 	public User() {
 	}
 
+	//TODO:Éú³ÉgetterºÍsetter
 }
--- src/com/pims/models/UserManager.java
+++ src/com/pims/models/UserManager.java
@@ -1,5 +1,7 @@
 package com.pims.models;
 
+import java.sql.Connection;
+
 public class UserManager {
 	private User user;
 
@@ -7,9 +9,51 @@ public class UserManager {
 		// TODO Auto-generated constructor stub
 	}
 
-	//TODO 匹配用户名密码的方法。
-	public Boolean pwdRight(User user){
+	
+	/**
+	 * 从Servlet传递User过来，将数据存入数据库
+	 * @param user
+	 */
+	public void add(User user){
+	}
+
+
+	/**
+	 * 在Servlet端调用，根据传递过来的id查找特定的用户
+	 * @param id
+	 * @return 指定的用户
+	 */
+	public User findOne(Integer id){
+		return null;
+	}
+	
+	/**
+	 * 根据用户名查找用户
+	 * @return 用户
+	 */
+	public User findByUsername(String username){
+		return null;
+	}
+	
+	/**
+	 *  从Servlet传递user过来，将数据库内的用户内容修改。
+	 * @param user
+	 */
+	public void update(User user){
+		
+	}
+	
+	/**
+	 * 从Servlet传递User过来，从数据库查询账号密码是否相匹配。
+	 * @param user
+	 * @return 匹配返回true否则返回false
+	 */
+	public Boolean authUser(User user){
 		return false;
+	}
+	
+	public Connection getConncetion(){
+		return null;
 	}
 	
 }

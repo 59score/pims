65fe4f7da4b097f322ef4cf7824d5e65bad618b3
Rita Liang <viso.liang@qq.com>
2016-12-08 11:49:29 +0800

    添加用户Bean和管理Manager。

---
+++ src/com/pims/models/User.java
@@ -0,0 +1,10 @@
+package com.pims.models;
+
+public class User {
+	private String username;
+	private String password;
+
+	public User() {
+	}
+
+}
---
+++ src/com/pims/models/UserManager.java
@@ -0,0 +1,15 @@
+package com.pims.models;
+
+public class UserManager {
+	private User user;
+
+	public UserManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	//TODO 匹配用户名密码的方法。
+	public Boolean pwdRight(User user){
+		return false;
+	}
+	
+}

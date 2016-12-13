7db3058f1d8c6c650bb550c102904f6df2e70a20
Rita Liang <viso.liang@qq.com>
2016-12-13 20:53:57 +0800

    添加文件上传代码。

---
+++ src/com/pims/models/File.java
@@ -0,0 +1,36 @@
+package com.pims.models;
+
+public class File {
+
+	private Integer id;
+	private String name;
+	private String path;
+	
+	public File() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
